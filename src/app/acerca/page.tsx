33a87d2c7abd60a113fcfd5f8f98cd08b8da0d5a
Ethan7FJ <johanfetecua11@gmail.com>
2025-09-08 18:40:24 -0500

    Apartado de Inventario y acerca de establecidos con navegacion por medio del menubar, contenido proximamente, boton para cerrar sesion listo

--- src/app/acerca/page.tsx
+++ src/app/acerca/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Navbar, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
 import { useRouter } from "next/navigation";
 import api from "../Service/api";
 import { useState, useEffect } from "react";
-import { Navbar, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
 
-export default function DashboardPage() {
+import userIMG from "../Imgs/user.png"
+import Image from "next/image";
+
+export default function AcercaDe() {
     const [user, setUser] = useState<any>(null);
     const router = useRouter();
 
@@ -40,12 +43,12 @@ export default function DashboardPage() {
                 <Navbar position="static" className="rounded-sm border-b-1 border-r-1 border-gray-900/50 shadow-2xl shadow-gray-900/50">
                     <NavbarContent className="hidden sm:flex gap-4" justify="center">
                         <NavbarItem>
-                            <Link color="foreground" className="bg-gray-300/50 p-2 rounded-sm text-lg border-b-1 border-l-1 border-r-1 border-gray-600/50">
+                            <Link color="foreground" className="p-2 rounded-sm text-lg duration-300 hover:text-white hover:bg-gray-900/80" href="/dashboard">
                                 Inventrario
                             </Link>
                         </NavbarItem>
                         <NavbarItem>
-                            <Link color="foreground" className="p-2 rounded-sm text-lg duration-300 hover:text-white hover:bg-gray-900/80" href="/acerca">
+                            <Link color="foreground" className="bg-gray-300/50 p-2 rounded-sm text-lg border-b-1 border-l-1 border-r-1 border-gray-600/50" >
                                 Acerca de
                             </Link>
                         </NavbarItem>
@@ -64,20 +67,20 @@ export default function DashboardPage() {
                         </NavbarItem>
                     </NavbarContent>
                 </Navbar>
-                <h1 className="p-4 bg-gray-700/50 rounded-sm">{user.username}</h1>
+                <div className="p-4 bg-gray-700/50 rounded-sm flex flex-col items-center">
+                    <Image
+                        src={userIMG}
+                        alt="Foto de perfil"
+                        width={50}          // ancho en px
+                        height={50}         // alto en px
+                        className="rounded-full"
+                    />
+                    {/* <h1 className="">{user.username}</h1> */}
+                </div>
             </header>
-            <p className="">Bienvenido {user.username}</p>
-
-            <footer>
-                <Button
-                    onPress={() => {
-                        localStorage.removeItem("token");
-                        router.push("/");
-                    }}
-                >
-                    Cerrar sesión
-                </Button>
-            </footer>
+            <p>
+                oda
+            </p>
         </div>
-    );
+    )
 }
